050506cb8596cd9755e674eaf4c793ebc7518547
Ledchig <s9826059221@gmail.com>
2023-06-22 16:16:01 +0500

    update locales for ru

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,13 +1,13 @@
 export default {
   translation:{
     errors: {
-      dublUrl: 'Такой RSS уже добавлен :)',
-      invalidUrl: 'Ссылка должна быть валидным URL`ом.',
-      notRSS: 'По ссылке не нашлось валидного RSS :(',
-      networkError: 'Беда с сетью. Попробуй еще раз!',
+      dublUrl: 'RSS уже существует',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      notRSS: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
     },
-    addedUrl: 'RSS успешно загружен!',
-    sending: `Запрос выполняется, ждём.`,
+    addedUrl: 'RSS успешно загружен',
+    sending: '',
     items: {
       feeds: 'Фиды',
       posts: 'Посты',
